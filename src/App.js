115230c6be10a14616e734fb27cc834f1a9da3c4
Tim Ruffles <truffles@plaid.com>
2020-04-08 15:58:53 +0100

    reproduce bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {usePlaidLink} from "react-plaid-link";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {open, ready, error} = usePlaidLink({
+        clientName: 'Your app name',
+        env: 'sandbox',
+        product: ['auth', 'transactions'],
+        publicKey: '48e057614f6b02d8dbecdf4ec40fc9',
+        onSuccess: (...args) => {
+            console.log(...args)
+        },
+    });
+
+    const [count, setCount] = useState(0)
+
+    // render really fast
+    useEffect(() => {
+        window.requestAnimationFrame(() => {
+            setCount(count + 1)
+        })
+    })
+
+    return (
+        <div className="App">
+
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo two"/>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={open}
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
